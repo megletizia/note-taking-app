db96dddbbd5bfff16d7369065be51d58e9d74a79
Meghan Letizia <megletizia@gmail.com>
2019-03-23 19:26:08 -0400

    Added bootstrap and button to add a note

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,18 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Note Taking Application by Meghan Letizia</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/megletizia/note-taking-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Github Link
           </a>
         </header>
+        <div>
+          <button>Add Note</button>
+        </div>
       </div>
     );
   }
