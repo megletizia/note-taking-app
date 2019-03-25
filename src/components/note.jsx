6de84c3cfd5efc5c7e50d76b35d4fbcb38653c9d
Meghan Letizia <megletizia@gmail.com>
2019-03-24 23:16:52 -0400

    Removed sub-title from notes

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -8,7 +8,6 @@ class Note extends Component {
       <div className="card col-sm-6 col-lg-4">
         <div className="card-body">
           <h5 className="card-title">Note title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Note subtitle</h6>
           <p className="card-text">Note Body</p>
           <EditNote />
           <button
