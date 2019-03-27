4ddc8a38c0045662cbbc78bb3f414d17a258aa6f
Meghan Letizia <megletizia@gmail.com>
2019-03-27 00:22:06 -0400

    Updating styling and adding comments

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ class App extends Component {
   state = {
     notes: [{ id: 1, content: "Example Note!" }]
   };
+  // Delete function for when a user presses the close icon on a note
   deleteNote = id => {
     // Filters out the notes that were clicked on
     const notes = this.state.notes.filter(note => {
+      // returns all the notes that don't have the same id as the one clicked
       return note.id !== id;
     });
     this.setState({
       notes
     });
   };
+  // Add function for when a user presses the add icon on a note or types in the form
   addNote = note => {
+    // Gives the new added notes a random id
     note.id = Math.random();
     let notes = [...this.state.notes, note];
     this.setState({ notes });
   };
   render() {
     return (
+      // Front page of application when it is first opened
       <div className="container">
-        <h1 className="cw title">Note Taking Application</h1>
-        <p className="white-text">
+        <h1 className="cw">Note Taking Application</h1>
+        <p className="card intro">
           This application will allow users to create, read, update and delete
           notes. You can start by adding a note by typing in the note ahead of
           time on the line below and pressing the enter key or pressing the add
